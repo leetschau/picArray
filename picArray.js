d0228970be4243081d2f44000a5e6c91af8dd7f2
leetschau <leetschau@gmail.com>
2015-05-28 17:47:26 +0800

    simple add and list
    Signed-off-by: leetschau <leetschau@gmail.com>

--- picArray.js
+++ picArray.js
@@ -1,23 +1,13 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.newbook.helpers({
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.booklist.helpers({
+    allBooks: function() {
+      return Books.find();
+    },
+    getCover: function() {
+      return this.cover;
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
